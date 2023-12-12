d2311d1c1f421356d0a34cdf08e599ecc97be854
daltonmcphaden <dalton.mcphaden@hotmail.com>
2023-12-11 20:49:39 -0500

    chore: add react native paper ui lib

--- apps/client/src/app/App.tsx
+++ apps/client/src/app/App.tsx
@@ -1,13 +1,19 @@
 import React from "react"
 import { View, Text } from "react-native"
 import { Link } from "expo-router"
+import { Button, useTheme } from "react-native-paper"
 
 export const App = () => {
+  const theme = useTheme()
+
   return (
     <>
-      <View>
+      <View style={{ backgroundColor: theme.colors.primaryContainer }}>
         <Text>This is the App page</Text>
         <Link href="/about">About</Link>
+        <Button icon="camera" mode="contained" onPress={() => console.log("Pressed")}>
+          Press me
+        </Button>
       </View>
     </>
   )
