91c3e450f7b63213e5c030f632ef64c088a5d99f
daltonmcphaden <dalton.mcphaden@hotmail.com>
2024-01-06 14:05:12 -0500

    react + mui + ts setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import { Button } from "@mui/material"
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Button variant="contained">Hello world</Button>
+      </div>
+      <img src={"../public/icon.png"}></img>
+    </>
+  )
+}
+
+export default App
